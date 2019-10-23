7be484d7ef5cdf45d784086114fc4e29602ca5c2
sreekanthssr <sreekanthssr@gmail.com>
2019-10-23 22:14:41 +0530

    Updates

---
+++ precache-manifest.a95f7a00ebe25e5be5e1d7a91614c1e3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b46a05eee21b9ed63476aa762bd881",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b0fd5201a2761a314fc",
+    "url": "/static/css/main.a8178c23.chunk.css"
+  },
+  {
+    "revision": "7489d0611bb39682afe1",
+    "url": "/static/js/2.4fbaf270.chunk.js"
+  },
+  {
+    "revision": "6b0fd5201a2761a314fc",
+    "url": "/static/js/main.c679682a.chunk.js"
+  },
+  {
+    "revision": "76c745c123e16de1862b",
+    "url": "/static/js/runtime-main.643855ea.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "04c4aeb3af61f1966e1c0b89b8c5c937",
+    "url": "/static/media/ruled.04c4aeb3.svg"
+  }
+]);
