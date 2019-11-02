41bb4d4bf0d6ac7af2d70bbcab8d915ec65f7e1d
sreekanth s <sreekanthssr@gmail.com>
2019-11-02 21:25:34 +0530

    removed unwanted code

--- src/lib/fabric-undo-redo.js
+++ src/lib/fabric-undo-redo.js
@@ -6,29 +6,6 @@ export default class fabricUndoRedo {
         this.options = {};
         this.options.canvas = (options.canvas) ? options.canvas : null;
         this.redoArray = [];
-        //this._setMouseHandler();
-    }
-
-    _setMouseHandler(){
-        this.options.canvas.includeDefaultValues = false;
-        /*this.options.canvas.on("mouse:down", () =>{
-        });*/
-        // let path = new fabric.Path('M 187.72019659936237 137.68010092291348 Q 187.71519659936237 137.68010092291348 187.50041948654845 137.68010092291348 ', {
-        //     stroke: 'red',
-        //     strokeWidth: 1,
-        //     fill: false,
-        //     originX: 'left',
-        //     originY: 'top'
-        // });
-        // path.set({ left: 100, top: 100 });
-        // this.options.canvas.add(path);
-        this.options.canvas.on("path:created", (e) =>{
-            // let objs = this.options.canvas.getObjects().map(function(o) {
-            //     return o;
-            //   });
-            //   if(objs[0].path)
-            console.log(this.options.canvas.toJSON());
-        });
     }
 
     action(type){
@@ -52,7 +29,6 @@ export default class fabricUndoRedo {
 
     _redo(){
         if(this.redoArray && this.redoArray.length){
-            console.log(this.redoArray);
             this.options.canvas.add(this.redoArray.pop());
             this.options.canvas.renderAll();
         }
